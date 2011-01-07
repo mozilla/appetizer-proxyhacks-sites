123e6525566b1c9e43d15d6a5396f18d182c020e
Ian Bicking <ianb@colorstudy.com>
2011-01-07 13:41:57 -0600

    more redditing

--- sites/reddit.colorstudy.com/rewriter.py
+++ sites/reddit.colorstudy.com/rewriter.py
@@ -4,6 +4,7 @@ from proxyhack.rewriting import add_head
 def rewriter(req, resp):
     resp = add_head(req, resp, '''
     <script src="/appetizer.js"></script>
+    <script src="http://myapps.mozillalabs.com/jsapi/include.js"></script>
     ''')
     resp = add_head(req, resp, '''
     <link rel="stylesheet" type="text/css" href="/appetizer.css" />
