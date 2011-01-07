3182baf70c0519be781d97e1651ac33ffaa1d5bd
Ian Bicking <ianb@colorstudy.com>
2011-01-07 14:27:14 -0600

    use stage

--- sites/reddit.colorstudy.com/rewriter.py
+++ sites/reddit.colorstudy.com/rewriter.py
@@ -4,7 +4,7 @@ from proxyhack.rewriting import add_head
 def rewriter(req, resp):
     resp.body += '''
     <script src="/appetizer.js"></script>
-    <script src="http://myapps.mozillalabs.com/jsapi/include.js"></script>
+    <script src="http://stage.myapps.mozillalabs.com/jsapi/include.js"></script>
     <link rel="stylesheet" type="text/css" href="/appetizer.css" />
     '''
     return resp
