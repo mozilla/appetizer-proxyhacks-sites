05173484431bfb923716ff9bb347b87dbb52ca71
Ian Bicking <ianb@colorstudy.com>
2011-01-07 14:57:46 -0600

    local repo

--- sites/reddit.colorstudy.com/rewriter.py
+++ sites/reddit.colorstudy.com/rewriter.py
@@ -1,11 +1,9 @@
-from proxyhack.rewriting import add_head
-
-
 def rewriter(req, resp):
     resp.body += '''
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
     <script src="/appetizer.js"></script>
-    <script src="http://stage.myapps.mozillalabs.com/jsapi/include.js"></script>
+    <!-- <script src="http://stage.myapps.mozillalabs.com/jsapi/include.js"></script> -->
+    <script src="http://localserver/jsapi/include.js"></script>
     <link rel="stylesheet" type="text/css" href="/appetizer.css" />
     '''
     return resp
