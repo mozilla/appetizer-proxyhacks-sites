1eafbe4f0b729fc99a9afee4d7a1f9f3b38b8623
root <root@proxyhacks.(none)>
2011-01-20 21:39:00 +0000

    updates to actually make things work

--- global_static/appetizer.js
+++ global_static/appetizer.js
@@ -1,31 +1,42 @@
-jQuery(function ($) {
-
-  var body = $('body');
-  if (! body.length) {
-    body = $(document);
+var onReady = function () {
+  if (! navigator.apps) {
+    // Hasn't loaded for some reason :P
+    setTimeout(onReady, 100);
+    return;
   }
+
+  var body = document.getElementsByTagName('body')[0];
   navigator.apps.getInstalled(function (v) {
     if (! v.length) {
       // The app is not yet installed
-      var div = $('<div class="appetizer"><a href="#" class="close">&#215;</a> <a href="#" class="install">Install this application!</a></div>');
-      $('a.install', div).click(function () {
+      var div = document.createElement('div');
+      div.className = 'appetizer';
+      div.innerHTML = '<a href="#" class="close">&#215;</a>' +
+        '<a href="#" class="install">Install this application!</a>';
+      var close = div.getElementsByTagName('a')[0];
+      var install = div.getElementsByTagName('a')[1];
+      close.addEventListener('click', function () {
+        div.style.display = 'none';
+        return false;
+      }, false);
+      install.addEventListener('click', function () {
         navigator.apps.install({
-          url: '/manifest.json',
+          url: location.protocol + '//' + location.host + '"/manifest.json",
           callback: function () {
-            div.text('Installed!');
-            div.addClass('success');
+            install.innerHTML = "Installed!";
+            div.className += ' success';
           }
         });
         return false;
-      });
-      $('a.close', div).click(function () {
-        div.remove();
-        return false;
-      });
-      body.prepend(div);
+      }, false);
+      body.insertBefore(div, body.childNodes[0]);
     }
   });
+};
 
-});
+if (document.readyState == 'complete' || true) {
+  onReady();
+} else {
+  document.addEventListener("load", onReady, false);
+}
 
-jQuery.noConflict(true);
